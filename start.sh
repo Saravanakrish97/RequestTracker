0aff778476aca18c17c2601851518c08252adeef
Saravanakrish97 <95554319+Saravanakrish97@users.noreply.github.com>
2022-03-11 00:00:11 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Saravanakrish97/RequestTraker.git /RequestTraker
+  git clone https://github.com/Saravanakrish97/RequestTracker.git /RequestTracker
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /RequestTraker
+  git clone $UPSTREAM_REPO /RequestTracker
 fi
-cd /RequestTraker
+cd /RequestTracker
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
